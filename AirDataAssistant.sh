8a7ce5c4694469e349d618d94a3ff688e70cf518
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-14 17:06:06 -0300

    add: .jar and shell script

---
+++ AirDataAssistant.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+PURPLE='0;35'
+NC='\033[0m' 
+VERSAO=11
+
+clear
+echo  "$(tput setaf 10)[AirData assistant]:$(tput setaf 7) Olá $USER, sou seu assistente. !;"
+echo  "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Verificando se você possui o Java instalado...;"
+sleep 2
+
+which java
+if [ $? -eq 0 ]
+	then
+		echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7) : Você já tem o java instalado!!!"
+		sleep 2 
+		clear
+	else
+		echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Nenhuma versão do java encontrada!"
+		echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Você deseja instalar o Java (S/N)?"
+		read inst
+		clear
+		if [ \"$inst\" == \"S\" ]
+			then
+				echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Ok, Iniciando a instalação do Java!"
+				echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Adicionando o repositório!"
+				sleep 2
+				sudo add-apt-repository ppa:webupd8team/java -y
+				clear
+				echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Atualizando! Quase lá."
+				sleep 2
+				sudo apt update -y
+				clear
+				
+				if [ $VERSAO -eq 11 ]
+					then
+						echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7) Preparando para instalar a versão 11 do Java."
+						sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
+						clear
+						echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7) Java instalado com sucesso!"
+				fi
+			else 	
+				echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Você optou por não instalar o Java por enquanto."
+				sleep 2
+				clear
+			
+		fi
+fi
+			
+echo "$(tput setaf 10)[AirData assistant]:$(tput setaf 7)  Encerrando, até mais!"
+
+# ===================================================================
+# Todos direitos reservados para o autor: Dra. Profa. Marise Miranda.
+# Sob licença Creative Commons @2020
+# Podera modificar e reproduzir para uso pessoal.
+# Proibida a comercialização e a exclusão da autoria.
+# ===================================================================
