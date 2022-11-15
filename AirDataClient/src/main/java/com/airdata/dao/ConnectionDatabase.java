443ef706b7f52ba68155e42cfdb3ef990fe9d084
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-14 21:02:18 -0300

    change: porta de conexão com o banco

--- AirDataClient/src/main/java/com/airdata/dao/ConnectionDatabase.java
+++ AirDataClient/src/main/java/com/airdata/dao/ConnectionDatabase.java
@@ -11,7 +11,7 @@ public class ConnectionDatabase {
         Connection conn = null;
         
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
