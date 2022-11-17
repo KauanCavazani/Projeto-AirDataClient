7fe04c95e6ee7d890c1fc30bced7d693fb8a380f
KauanCavazani <kauan.cavazani@gmail.com>
2022-11-16 21:43:58 -0300

    add: metodo getIpv4

--- AirDataClient/src/main/java/com/airdata/dao/ConnectionDatabase.java
+++ AirDataClient/src/main/java/com/airdata/dao/ConnectionDatabase.java
@@ -1,5 +1,7 @@
 package com.airdata.dao;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +11,7 @@ public class ConnectionDatabase {
     public Connection getConnectionMYSQL() {
         
         Connection conn = null;
+        String ipv4 = getIpv4();
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,7 +20,7 @@ public class ConnectionDatabase {
         }
         
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://35.172.203.203/airData", "airdata_client", "#AirData2022");
+            conn = DriverManager.getConnection("jdbc:mysql://" + ipv4 + "/airData", "airdata_client", "#AirData2022");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -42,6 +45,20 @@ public class ConnectionDatabase {
         }
         
         return conn;
+    }
+    
+    private String getIpv4() {
+        
+        String ipv4 = null;
+        
+        try {
+            InetAddress thisIp = InetAddress.getLocalHost();
+            ipv4 = thisIp.getHostAddress().toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        System.out.println(ipv4);
+        return ipv4;
     }
     
 }
